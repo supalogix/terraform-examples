ae2f47ea1175256ecabf20a0362a485124e2a07f
Jonathan Nacionales <jnaciona@gmail.com>
2018-04-21 16:10:31 -0700

    Update user-data

--- clustered-servers/user-data.sh
+++ clustered-servers/user-data.sh
@@ -2,6 +2,11 @@
 
 sudo apt-get update -y
 sudo apt-get install -y python
+sudo apt-get install -y docker.io
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 cd /home/ubuntu
-echo "Hello World" > /home/ubuntu/index.html
-sudo python -m SimpleHTTPServer 80
+git init
+git remote add origin https://github.com/supalogix/node-hello-world.git
+git pull origin master
+docker-compose up -d
